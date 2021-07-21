ef80e3489106f71a03c168fb51c4112ca0dc11b8
Gervaise H. Henry <gervaise.henry@gmail.com>
2021-07-20 23:28:11 -0500

    Comment out col assertion

--- cellxgene_schema_cli/cellxgene_schema/remix.py
+++ cellxgene_schema_cli/cellxgene_schema/remix.py
@@ -211,7 +211,7 @@ def fixup_gene_symbols(adata, fixup_config):
             df = adata.layers[layer]
         merged_df = merge_df(df, domain, adata.obs.index, upgraded_var_index)
         assert merged_df.index.equals(merged_X.index)
-        assert merged_df.columns.equals(merged_X.columns)
+        #assert merged_df.columns.equals(merged_X.columns)
 
         if domain == "raw":
             fixup_raw = anndata.AnnData(
